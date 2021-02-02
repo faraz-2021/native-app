3df6a5ae8cf940c2d2fff82b63c760105599b1c7
faraz-2021 <faraz@excellencetechnologies.in>
2021-02-02 16:10:08 +0530

    native app completed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { setStatusBarBackgroundColor, StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import TodoApp from "./src/components/TodoApp";
@@ -7,72 +7,60 @@ import { Colors } from "./src/components/color/color";
 import Constants from "expo-constants";
 import { NavigationContainer } from "@react-navigation/native";
 
-function HomeScreen({ addTodos, todo, setCheck }) {
+function HomeScreen({ addTodos,todo ,setCheck}) {
   return (
     <ScrollView>
       <View>
-        <TodoApp
-          type="Today"
-          addTodos={addTodos}
-          todo={todo}
-          setCheck={setCheck}
-        />
+        <TodoApp type="Today" addTodos={addTodos} todo={todo} setCheck={setCheck}/>
       </View>
     </ScrollView>
   );
 }
-function Active({ addTodos, todo, setCheck }) {
+function Active({addTodos,todo ,setCheck }) {
   const [active, setActive] = useState([]);
-  useEffect(() => {
+  useEffect(()=>{
     ActiveList();
-  }, [ActiveList]);
+  },[ActiveList])
 
-  const ActiveList = () => {
-    const Comp = todo.filter((item) => {
-      if (!item.check) {
-        return item;
+
+  const ActiveList = () =>{
+    const Comp = todo.filter((item)=>{
+      if(!item.check){
+        return item
       }
-    });
-    setActive(Comp);
-  };
+    })
+    setActive(Comp)
+  }
   return (
     <ScrollView>
       <View>
-        <TodoApp
-          type="Active"
-          addTodos={addTodos}
-          todo={active}
-          setCheck={setCheck}
-        />
+        <TodoApp type="Active" addTodos={addTodos} todo={active} setCheck={setCheck} />
       </View>
     </ScrollView>
   );
 }
 
-function Completed({ addTodos, todo, setCheck }) {
-  const [complete, setComplete] = useState([]);
-  useEffect(() => {
-    CompleteList();
-  }, [CompleteList]);
+function Completed({addTodos,todo,setCheck}) {
 
-  const CompleteList = () => {
-    const Comp = todo.filter((item) => {
-      if (item.check) {
-        return item;
+  const [complete, setComplete] = useState([]);
+  useEffect(()=>{
+    CompleteList();
+  },[CompleteList])
+
+
+  const CompleteList = () =>{
+    const Comp = todo.filter((item)=>{
+      if(item.check){
+        return item
       }
-    });
-    setComplete(Comp);
-  };
+    })
+    setComplete(Comp)
+  }
 
   return (
     <ScrollView>
       <View>
-        <TodoApp
-          type="Completed"
-          addTodos={addTodos}
-          todo={complete}
-          setCheck={setCheck}
-        />
+        <TodoApp type="Completed" addTodos={addTodos} todo={complete} setCheck={setCheck} />
       </View>
     </ScrollView>
   );
@@ -80,7 +68,10 @@ function Completed({ addTodos, todo, setCheck }) {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   const [todo, setTodo] = useState([]);
+
+
 
   const addTodos = (text) => {
     if (text.trim().length > 0) {
@@ -90,13 +81,14 @@ export default function App() {
           { title: text.trim(), key: Math.random().toString(), check: false },
         ];
       });
+     
     } else {
       Alert.alert("error!", "List can't be empty", [
         { text: "Ok", onPress: () => console.log("Alert Closed") },
       ]);
     }
   };
-
+  
   const setCheck = (key) => {
     const newList = todo.map((e) => {
       if (e.key === key) {
@@ -110,31 +102,26 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          labelStyle: { textTransform: "none", fontSize: 20, marginRight: 10 },
-          tabStyle: {
-            width: "auto",
-            alignItems: "center",
-            justifyContent: "center",
-          },
-        }}
-      >
-        <Tab.Screen name="All">
-          {() => (
-            <HomeScreen addTodos={addTodos} todo={todo} setCheck={setCheck} />
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="Active">
-          {() => <Active addTodos={addTodos} todo={todo} setCheck={setCheck} />}
-        </Tab.Screen>
-        <Tab.Screen name="Completed">
-          {() => (
-            <Completed setCheck={setCheck} addTodos={addTodos} todo={todo} />
-          )}
-        </Tab.Screen>
+      <Tab.Navigator 
+      tabBarOptions={{
+        activeTintColor: Colors.red,
+        inactiveTintColor: Colors.black,
+        labelStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+          padding: 1,
+          marginBottom: 10,
+        },
+      }}>
+        <Tab.Screen name ="All" component={() => <HomeScreen  addTodos={addTodos} todo={todo} setCheck={setCheck}/>} />
+        <Tab.Screen
+          name="Active"
+          component={() => <Active  addTodos={addTodos} todo={todo} setCheck={setCheck}/>}
+        />
+        <Tab.Screen name="Completed"  component={() => <Completed  addTodos={addTodos} todo={todo} setCheck={setCheck}/>}/>
       </Tab.Navigator>
-      <StatusBar style="auto"/>
     </NavigationContainer>
   );
 }
+
+
